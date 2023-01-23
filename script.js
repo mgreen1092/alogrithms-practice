8603aa5314b8e79a08acf6d4a7087e1548ad4a90
mgreen1092 <morgan.green0209@gmail.com>
2023-01-23 11:32:22 -0500

    areThereDuplicates Instructions

--- script.js
+++ script.js
@@ -148,5 +148,10 @@ function sameFrequency(num1, num2) {
        return true
     }
 }
-console.log(sameFrequency(182,281))
-console.log(sameFrequency(34,14))
+// console.log(sameFrequency(182,281))
+// console.log(sameFrequency(34,14))
+
+// Implement a function called, areThereDuplicates which accepts a variable 
+// number of arguments, and checks whether there are any duplicates among 
+// the arguments passed in.  You can solve this using the frequency counter 
+// pattern OR the multiple pointers pattern.
