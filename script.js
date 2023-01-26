7d42da4a01bd60be788e7fb75c727827e2c8eb2f
mgreen1092 <morgan.green0209@gmail.com>
2023-01-26 10:33:48 -0500

    recursion power function instructions

--- script.js
+++ script.js
@@ -377,4 +377,10 @@ function factorial (num) {
     if (num === 1) return 1
     return num * factorial(num - 1)
 }
-console.log(factorial(4))
+// console.log(factorial(4))
+
+// Recursion - Power:
+// Write a function called power which accepts a base and an exponent. The function should return 
+// the power of the base to the exponent. This function should mimic the functionality of 
+// Math.pow()  - do not worry about negative bases and exponents.
+
