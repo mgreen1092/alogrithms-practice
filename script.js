10b8765d7e683d9509138b4c0c442e6d2781c91a
mgreen1092 <morgan.green0209@gmail.com>
2023-01-30 10:21:11 -0500

    reduced white space of binary search

--- script.js
+++ script.js
@@ -450,18 +450,11 @@ function binarySearch (arr, val) {
     let end = arr.length - 1
     let middle = Math.floor((start + end)/2)
     while (arr[middle] !== val && start <= end) {
-        if (val < arr[middle]) {
-            end = middle + 1
-        } else {
-            start = middle + 1
-        }
+        if (val < arr[middle]) end = middle + 1
+        else start = middle + 1
         middle = Math.floor((start+end)/2)
     }
-    // console.log(start, middle, end)
-    if (arr[middle] === val){
-        return middle
-    } else {
-        return -1 
+    return arr[middle] === val ? middle : -1 
     }
     // let left = 0
     // let right = arr.length - 1
