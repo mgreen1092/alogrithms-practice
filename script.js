d936a6daecf4a4a3ce779fa8dffaaeb45acea64b
mgreen1092 <morgan.green0209@gmail.com>
2023-01-30 09:23:03 -0500

    completed linear search problem

--- script.js
+++ script.js
@@ -417,4 +417,20 @@ const formatDate = (dateInput) => {
     // conjoin year month day to correct format, need to include -
     return [year, month, day].join('-')
   }
-console.log(formatDate("3/17/1888"))  
+// console.log(formatDate("3/17/1888"))  
+
+// Linear Search
+// function accepts an array and a value 
+// loop through the array and check if the current array 
+// element is equal to the value
+// if it is, return the index at which the element is found
+
+function linearSearch(arr, num) {
+    for (let i=0; i<=arr.length; i++) {
+        if (arr[i] === num) {
+            return arr.indexOf(num)
+        } 
+        return -1
+    }
+}
+console.log(linearSearch([1, 2, 3, 55], 55))
