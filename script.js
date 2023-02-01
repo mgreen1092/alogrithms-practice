c7f48f664f65a2ad642c58b0fcd030445b0284bb
mgreen1092 <morgan.green0209@gmail.com>
2023-02-01 10:02:48 -0500

    draft bubble sort function

--- script.js
+++ script.js
@@ -504,3 +504,18 @@ function stringSearch (string, val) {
 // start an inner loop with a variable called j from the beginning until i-1
 // if arr[j] is greater than arr[j+1], swap those two values 
 // return sorted array
+
+function bubbleSort (arr) {
+    for (let i=arr.length - 1; i>=0; i--) {
+        for (let j=0; j<i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                temp = arr[j+1]
+            }
+        }
+    }
+    return arr
+}
+
+console.log(bubbleSort([5, 4, 3, 2, 1]))
