6c07ca47081c38b60c79830aca7b6a165892a186
mgreen1092 <morgan.green0209@gmail.com>
2023-02-01 10:16:34 -0500

    updated bubble sort function

--- script.js
+++ script.js
@@ -505,9 +505,24 @@ function stringSearch (string, val) {
 // if arr[j] is greater than arr[j+1], swap those two values 
 // return sorted array
 
+// function bubbleSort (arr) {
+//     for (let i=0; i<arr.length; i++) {
+//         for (let j=0; j<arr.length; j++) {
+//             if (arr[j] > arr[j+1]) {
+//                 console.log(arr[j], arr[i])
+//                 // Swap
+//                 let temp = arr[j]
+//                 arr[j] = arr[j+1]
+//                 arr[j+1] = temp
+//             }
+//         }
+//     }
+//     return arr
+// }
 function bubbleSort (arr) {
-    for (let i=0; i<arr.length; i++) {
-        for (let j=0; j<arr.length; j++) {
+    for (let i=arr.length; i>0; i--) {
+        for (let j=0; j<i-1; j++) {
+            // this allows the loop to run fewer times
             if (arr[j] > arr[j+1]) {
                 console.log(arr[j], arr[i])
                 // Swap
