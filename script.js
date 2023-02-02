bdf973d94918e53cdee97730c4b3c75aee017ccc
mgreen1092 <morgan.green0209@gmail.com>
2023-02-02 11:46:07 -0500

    correct maxProfit function

--- script.js
+++ script.js
@@ -590,5 +590,20 @@ function insertionSort(arr) {
 // Sliding window example
 
 function maxProfit (prices) {
-    
-}
+    let left = 0; // Buy
+    let right = 1; // sell
+    let max_profit = 0;
+    while (right < prices.length) {
+      if (prices[left] < prices[right]) {
+        let profit = prices[right] - prices[left]; // our current profit
+  
+        max_profit = Math.max(max_profit, profit);
+      } else {
+        left = right
+      }
+      right++;
+    }
+    return max_profit;
+  };
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]))
