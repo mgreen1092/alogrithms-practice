a28c201eb6cc7dd1ad497b2ddd5e73e632e30297
mgreen1092 <morgan.green0209@gmail.com>
2023-02-07 10:13:36 -0500

    maxDigits function

--- script.js
+++ script.js
@@ -758,3 +758,9 @@ function digitCount (num) {
     if (num === 0) return 1
     return Math.floor(Math.log10(Math.abs(num))) + 1
 }
+function mostDigits (arr) {
+    let maxDigits = 0
+    for (let i=0; i<arr.length; i++) {
+        maxDigits = Math.max(maxDigits, digitCount(arr[i]))
+    }
+}
