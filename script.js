62eb5f0abedea0d4cde7f82be2aaeb48c0b12345
mgreen1092 <morgan.green0209@gmail.com>
2023-02-07 12:00:18 -0500

    complete threesum

--- script.js
+++ script.js
@@ -792,3 +792,39 @@ function radixSort(nums) {
 // ThreeSum
 // Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] 
 // such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+function threeSum (nums) {
+    // need to identify three pointers
+    // pointer values cannot equal each other
+    // if the combination does not equal 0, adjust the pointers 
+    // return array of values that add to 0, can be more than one array
+    const results = []
+    if (nums.length < 3) return results
+    nums = nums.sort((a, b) => a - b)
+    console.log(nums)
+    let target = 0
+    for (let i=0; i<nums.length; i++) {
+        if (nums[i] > target) break
+        if (i > 0 && nums[i] === nums[i-1]) continue
+        let j=i+1
+        let k=nums.length - 1
+        while (j<k) {
+            let sum = nums[i] + nums[j] + nums[k]
+            if (sum === target) {
+                results.push([nums[i], nums[j], nums[k]])
+                while(nums[j] === nums[j + 1]) j++
+                while(nums[k] === nums[k-1]) k++
+                j++
+                k--
+            } else if (sum < target) {
+                j++
+            } else {
+                k--
+            }
+        }
+    }
+  return results
+   
+}
+
+console.log(threeSum([-1,0,1,2,-1,-4]))
